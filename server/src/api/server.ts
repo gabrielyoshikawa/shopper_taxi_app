7dd2263d40b6ab284a93579d35942efc2d4b90b6
Gabriel Yoshikawa Koti <105952251+gabrielyoshikawa@users.noreply.github.com>
2024-11-27 10:46:03 -0300

    FEAT: Create server

---
+++ server/src/api/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import { apiRouter } from "./router.js";
+
+const app = express();
+
+app.use(cors({
+	origin: "http://localhost:5173",
+	methods: "GET,POST,PUT,DELETE,PATCH",
+	allowedHeaders: "Content-Type,Authorization"
+}));
+
+const PORT = 8080;
+
+app.use(express.json());
+app.use("/api", apiRouter);
+
+app.listen(PORT, () => {
+	console.log(`App listening on http://localhost:${PORT}/`);
+});
