9631fca10349f51f8a6a0eb742b992125b8e8f6b
Gabriel Yoshikawa Koti <105952251+gabrielyoshikawa@users.noreply.github.com>
2024-11-27 20:10:26 -0300

    FEAT: Add App.tsx

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,26 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import RegisterForm from './modules/register/RegisterForm';
+import RequestRide from './modules/core/requestRide/RequestRide';
+import Menu from './modules/menu/Menu';
+import LoginForm from './modules/login/LoginForm';
+import RideHistory from './modules/core/rideHistory/RideHistory';
 
 function App() {
 
-  return (
-    <>
-	<h1>Hello world!</h1>
-    </>
-  )
+	return (
+		<>
+		<Router>
+			<Routes>
+				<Route path="/" element={<LoginForm />}></Route>
+				<Route path="/register" element={<RegisterForm />}></Route>
+				<Route path="/menu" element={<Menu />}></Route>
+				<Route path="/request" element={<RequestRide />}></Route>
+				<Route path="/history" element={<RideHistory />}></Route>
+			</Routes>
+		</Router>
+		</>
+	);
 }
 
-export default App
+export default App;
