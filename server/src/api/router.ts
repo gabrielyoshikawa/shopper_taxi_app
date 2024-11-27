147099643d87f71f784485400be9628c736a941c
Gabriel Yoshikawa Koti <105952251+gabrielyoshikawa@users.noreply.github.com>
2024-11-27 20:09:49 -0300

    FEAT: Add router

--- server/src/api/router.ts
+++ server/src/api/router.ts
@@ -1,8 +1,26 @@
 import { Router } from "express";
-import { Request, Response } from "express";
+import { InputValidator } from "./handlers/pipes/InputValidator.js";
+import { LoginController } from "./handlers/controllers/LoginController.js";
+import { RegisterUserController } from "./handlers/controllers/RegisterUserController.js";
+import { RideController } from "./handlers/controllers/RideController.js";
+import { DriverController } from "./handlers/controllers/DriverController.js";
 
 export const apiRouter = Router();
 
-apiRouter.get("/", (_req: Request, res: Response) => {
-	res.send("Hello World!");
-});
+apiRouter.route('/registerUser')
+	.post([InputValidator.userLoginOrRegisterRequest], RegisterUserController.registerNewUser);
+
+apiRouter.route('/login')
+	.post([InputValidator.userLoginOrRegisterRequest], LoginController.login);
+
+apiRouter.route('/ride/estimate')
+	.post([InputValidator.rideRequest], RideController.getRideInformation);
+
+apiRouter.route('/ride/confirm')
+	.patch([InputValidator.rideRequest, InputValidator.driverConfirmationRequest], RideController.confirmRide);
+
+apiRouter.route('/ride/:customer_id')
+	.get([InputValidator.validateUserRidesHistoryRequest], RideController.getUserRideHistory);
+
+apiRouter.route('/drivers')
+	.get(DriverController.getAllDrivers);
